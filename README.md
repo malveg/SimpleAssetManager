b90d156285add6b12e73ded551e0ae8476aceb4d
Gabe M <gabe.malveaux@gmail.com>
2019-11-17 12:31:45 -0500

    Added POST, PUT, DELETE methdods to assets route

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SimpleAssetManager
 An exploration in SQLite and Falcon to create a simple CRUD API
+
+## Setup
+> $pip install falcon
+
+### Install optional features
+> $pip install pytest
+> $pip install waitress
--- SimpleAssetManager/assets.py
+++ SimpleAssetManager/assets.py
@@ -15,13 +15,24 @@ class Resource(object):
         resp.status = falcon.HTTP_200
 
     def on_delete(self, req, resp):
+        payload = req.media.get('payload')
+        ids_to_delete = payload['ids_to_delete']
         al = AssetList()
-        idlist = range(2,50)
-        assets = al.RemoveAsset(idlist)
-        #get remaining
-        assets = al.GetAllAssets()
-        doc = {
-            'assets':  assets
-        }
-        resp.body = json.dumps(doc)
+        al.RemoveAsset(ids_to_delete)
+        resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        payload = req.media.get('payload')
+        names_to_add = payload['names']
+        al = AssetList()
+        for name in names_to_add:
+            al.AddAsset(name)
+        resp.status = falcon.HTTP_200
+
+    def on_put(self, req, resp):
+        payload = req.media.get('payload')
+        id = payload['id']
+        new_name = payload['new_name']
+        al = AssetList()
+        al.UpdateAsset(id,'name',new_name)
         resp.status = falcon.HTTP_200
